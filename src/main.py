ac9d40157d865e1beea14250fb3e2f460669f9e9
GeorgeKonrad29 <jorge.lopez2@utp.edu.co>
2026-04-06 19:28:25 -0500

    a

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
         "http://localhost:5173",  # Para Vite en desarrollo
     ],
     allow_origin_regex=r"https://front-onboarding(?:-[a-z0-9-]+)?\.vercel\.app",
-    allow_origin_regex=r"http://localhost:(3000|5173)(?:-[a-z0-9-]+)?\.vercel\.app",
+    allow_origin_regex=r"http://localhost:(3000|5173)(?:-[a-z0-9-]+)?",
     allow_credentials=True,
     allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Permite todos los headers
